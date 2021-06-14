f4796b17da0d8c4f6edde662ff8da0915322a3bc
Adam Komisarek <akakus@gmail.com>
2021-06-14 21:55:40 +0100

    Initial commit

---
+++ src/test/java/com/akomisarek/certtrackerservice/CertTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akomisarek.certtrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
